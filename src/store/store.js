435c00c87f7f4b1111ee70130bc061f3592ba156
your_name_here <mk@MKs-MacBook-Pro.local>
2019-08-29 08:12:40 -0600

    refactoring with Vuex

--- src/store/store.js
+++ src/store/store.js
@@ -1,17 +1,63 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { fetchNewsList } from "../api/index";
+import { fetchJobsList } from "../api/index";
+import { fetchAskList } from "../api/index";
+
+
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
     news: [],
+    jobs: [],
+    ask: [],
 
   },
   mutations: {
+    SET_NEWS(state, news){
+      state.news= news
+    },
+    SET_JOBS(state, jobs){
+      state.jobs= jobs
+    },
+    SET_ASK(state, ask){
+      state.ask= ask
+    },
+    
+
 
   },
   actions: {
+    FETCH_NEWS(context){
+      fetchNewsList()
+      .then(res=>{
+        context.commit('SET_NEWS', res.data)
+      })
+      .catch((err)=>{
+        console.log(err)
+      })
+    },
+    FETCH_JOBS({commit}){
+      fetchJobsList()
+      .then(({data}) => {
+        commit('SET_JOBS', data)
+      })
+      .catch(err => {
+        console.log(err.message);
+      });
+    },
+    FETCH_ASK({commit}){
+      fetchAskList()
+      .then(({data}) => {
+        commit('SET_ASK', data)
+      })
+      .catch(err => {
+        console.log(err.message);
+      });
+    }
+
 
   }
 })
--- src/views/AskView.vue
+++ src/views/AskView.vue
@@ -1,28 +1,16 @@
 <template>
   <div>
     <ul>
-      <li v-for="(item, index) in ask" :key="index"> {{ item.title }}</li>
+      <li v-for="(item, index) in this.$store.state.ask" :key="index"> {{ item.title }}</li>
     </ul>
   </div>
 </template>
 
 <script>
-import { fetchAskList } from "../api/index";
 export default {
-  data() {
-    return {
-      ask: []
-    };
-  },
+ 
   created() {
-    fetchAskList()
-      .then(res => {
-        console.log(res)
-        this.ask = res.data
-      })
-      .catch(err => {
-        console.log(err.message);
-      });
+    this.$store.dispatch('FETCH_ASK')
   }
 };
 </script>
