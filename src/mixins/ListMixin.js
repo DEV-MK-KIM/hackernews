9870be7c9afdf79f58baa62e2142e30268bad33c
MK Kim <mk@MKs-MacBook-Pro.local>
2019-08-30 19:49:20 -0600

    Router Promise

--- src/mixins/ListMixin.js
+++ src/mixins/ListMixin.js
@@ -1,20 +1,24 @@
 // MIXINS
 import bus from '../utils/bus'
 export default {
+
+    mounted(){
+        bus.$emit("end:spinner")
+    }
     // component
 
-    created(){
-        bus.$emit('start:spinner')
-        this.$store.dispatch("FETCH_LIST", this.$route.name)
-        // PROMISE CHAINING FROM VUEX ACTIONS (return res)
-        .then(()=>{
-            bus.$emit('end:spinner')
-            console.log(5)
-        })
-        .catch((err)=>{
-            console.log(err.message)
-        })
-    },
+    // created(){
+    //     bus.$emit('start:spinner')
+    //     this.$store.dispatch("FETCH_LIST", this.$route.name)
+    //     // PROMISE CHAINING FROM VUEX ACTIONS (return res)
+    //     .then(()=>{
+    //         bus.$emit('end:spinner')
+    //         console.log(5)
+    //     })
+    //     .catch((err)=>{
+    //         console.log(err.message)
+    //     })
+    // },
     
 }
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,57 +1,89 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import NewsView from '../views/NewsView'
-import JobsView from '../views/JobsView'
-import AskView from '../views/AskView'
-import UserView from '../views/UserView'
-import ItemView from '../views/ItemView'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import NewsView from '../views/NewsView';
+import JobsView from '../views/JobsView';
+import AskView from '../views/AskView';
+import UserView from '../views/UserView';
+import ItemView from '../views/ItemView';
 // import createListView from '../views/CreateListView'
 
+import bus from '../utils/bus';
+import store from '../store/store';
 
-
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 export default new VueRouter({
-    mode: 'history',
-    routes: [
-        {
-            path:'/',
-            redirect: 'news'
-        },
-        {
-            name:'news',
-            path:'/news',
-            // component: createListView('NewsView HOC'),
-            component: NewsView
-        },
-        {
-            name:'ask',
-            path:'/ask',
-            // component: createListView('AskView HOC')
-            component: AskView
-
-        },
-        {
-            name:'jobs',
-            path:'/jobs',
-            // component: createListView('JobsView HOC'),
-            component: JobsView
-
-        },
-        {
-            name:'user',
-            // '/user/:params'
-            path:'/user/:id',
-            component: UserView,
-        },
-        {
-            name:'item',
-            // '/user/:params'
-            path:'/item/:id',
-            component: ItemView,
-        },
-
-    ]
-
-})
-
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      redirect: 'news'
+    },
+    {
+      name: 'news',
+      path: '/news',
+      // component: createListView('NewsView HOC'),
+      component: NewsView,
+      beforeEnter: (to, from, next) => {
+        bus.$emit('start:spinner');
+        store
+          .dispatch('FETCH_LIST', to.name)
+          // PROMISE CHAINING FROM VUEX ACTIONS (return res)
+          .then(() => {
+            next();
+          })
+          .catch(err => {
+            console.log(err.message);
+          });
+      }
+    },
+    {
+      name: 'ask',
+      path: '/ask',
+      // component: createListView('AskView HOC')
+      component: AskView,
+      beforeEnter: (to, from, next) =>{
+        bus.$emit('start:spinner');
+        store
+          .dispatch('FETCH_LIST', to.name)
+          // PROMISE CHAINING FROM VUEX ACTIONS (return res)
+          .then(() => {
+            next();
+          })
+          .catch(err => {
+            console.log(err.message);
+          });
+      }
+    },
+    {
+      name: 'jobs',
+      path: '/jobs',
+      // component: createListView('JobsView HOC'),
+      component: JobsView,
+      beforeEnter: (to, from, next) =>{
+        bus.$emit('start:spinner');
+        store
+          .dispatch('FETCH_LIST', to.name)
+          // PROMISE CHAINING FROM VUEX ACTIONS (return res)
+          .then(() => {
+            next();
+          })
+          .catch(err => {
+            console.log(err.message);
+          });
+      }
+    },
+    {
+      name: 'user',
+      // '/user/:params'
+      path: '/user/:id',
+      component: UserView
+    },
+    {
+      name: 'item',
+      // '/user/:params'
+      path: '/item/:id',
+      component: ItemView
+    }
+  ]
+});
