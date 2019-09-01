849c64fbc8f50465c20476870496d66ae6dbcf0d
MK Kim <mk@MKs-MacBook-Pro.local>
2019-08-31 20:45:35 -0600

    touch up codes

--- src/api/index.js
+++ src/api/index.js
@@ -3,22 +3,15 @@ import axios from 'axios'
 const config = {baseUrl: `https://api.hnpwa.com/v0/`}
 
 // FETCH ALL LISTS FROM NEWS ASK AND JOBS
-function fetchList(pageName){
-    return axios.get(`${config.baseUrl}${pageName}/1.json`)
+async function fetchList(pageName){
+    try{
+    return await axios.get(`${config.baseUrl}${pageName}/1.json`)
+    }
+    catch(error){
+        console.log(error)
+    }
 }
 
-
-// function fetchNewsList(){
-//     return axios.get(`${config.baseUrl}news/1.json`)  
-// }
-
-// function fetchAskList(){
-//     return axios.get(`${config.baseUrl}ask/1.json`)  
-// }
-
-// function fetchJobsList(){
-//     return axios.get(`${config.baseUrl}jobs/1.json`)  
-// }
 
 function fetchUserList(userName){
     return axios.get(`${config.baseUrl}user/${userName}.json`)
--- src/store/actions.js
+++ src/store/actions.js
@@ -4,64 +4,74 @@ import { fetchUserList, fetchItemList, fetchList } from "../api/index";
 
 
 export default {
-    // FETCH_NEWS(context){
-    //   fetchNewsList()
-    //   .then(res=>{
-    //     context.commit('SET_NEWS', res.data)
-    //   })
-    //   .catch((err)=>{
-    //     console.log(err)
-    //   })
-    // },
-    // FETCH_JOBS({commit}){
-    //   fetchJobsList()
-    //   .then(({data}) => {
-    //     commit('SET_JOBS', data)
-    //   })
-    //   .catch(err => {
-    //     console.log(err.message);
-    //   });
-    // },
-    // FETCH_ASK({commit}){
-    //   fetchAskList()
-    //   .then(({data}) => {
-    //     commit('SET_ASK', data)
-    //   })
-    //   .catch(err => {
-    //     console.log(err.message);
-    //   });
-    // },
-    FETCH_USER({commit},user){
-        fetchUserList(user)
-        .then(({data}) => {
-            console.log(data)
-          commit('SET_USER', data)
-        })
-        .catch(err => {
-          console.log(err.message);
-        });
+    
+    // FETCH_USER({commit},user){
+    //     fetchUserList(user)
+    //     .then(({data}) => {
+    //         console.log(data)
+    //       commit('SET_USER', data)
+    //     })
+    //     .catch(err => {
+    //       console.log(err.message);
+    //     });
+    //   },
+
+      async FETCH_USER(context, user){
+        try{
+        const response = await fetchUserList(user)
+        context.commit('SET_USER', response.data)
+        }
+        catch(error){
+          console.log(error)
+        }
       },
-      FETCH_ITEM({commit},item){
-        fetchItemList(item)
-        .then(({data}) => {
-            console.log(data)
-          commit('SET_ITEM', data)
-        })
-        .catch(err => {
-          console.log(err.message);
-        });
+
+
+      // FETCH_ITEM({commit},item){
+      //   fetchItemList(item)
+      //   .then(({data}) => {
+      //       console.log(data)
+      //     commit('SET_ITEM', data)
+      //   })
+      //   .catch(err => {
+      //     console.log(err.message);
+      //   });
+      // },
+
+      async FETCH_ITEM({commit}, item){
+        try{
+        const {data} = await fetchItemList(item)
+        commit('SET_ITEM', data)
+        }
+        catch(error){
+          console.log(error)
+        }
       },
-      FETCH_LIST({commit},name){
-        // use return for promie chaning
-        return fetchList(name)
-        .then((res) => {
-          commit('SET_LIST', res.data)
-          console.log(4)
-          return res;
-        })
-        .catch(err => {
-          console.log(err.message);
-        });
-      },
+      // Promise
+
+      // FETCH_LIST({commit},name){
+      //   // use return for promie chaning
+      //   return fetchList(name)
+      //   .then((response) => {
+      //     commit('SET_LIST', response.data)
+      //     return response;
+      //   })
+      //   .catch(err => {
+      //     console.log(err.message);
+      //   });
+      // },
+
+      // Async/Await
+
+      async FETCH_LIST({commit}, name){
+        try{
+        const response = await fetchList(name)
+        commit('SET_LIST', response.data)
+        return response
+        }
+        catch(err){
+          console.log(err)
+        }
+      }
    
 }
